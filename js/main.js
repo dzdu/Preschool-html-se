496c4dfab32bb972852554afa300663e6eaa05da
instdeadangel <513dragan@gmail.com>
2020-09-13 13:12:42 +0200

    Created page with css and html so far, next commit will include js and maybe moved to react

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* Next commit fix */
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//     document.getElementById("header").style.fontSize = "30px";
+//     document.getElementById("header").style.backgroundImage = "none";
+//     document.getElementById("header").style.height = "200px";
+//     document.getElementById("b-header").style.display = "none";
+//   } else {
+//     document.getElementById("header").style.fontSize = "90px";
+//     document.getElementById("b-header").style.display = "block";
+//     document.getElementById("header").style.backgroundImage = "none";
+//   }
+// }
